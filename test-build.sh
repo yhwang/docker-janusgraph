6a551e4132fa26548ada9e0f50d29cee6f1a3e12
Yihong Wang <yh.wang@ibm.com>
2017-07-20 11:05:11 -0700

    Add janusgraph-console
    This docker image is used to run gremlin-console. The
    `/home/janusgraph/janusgraph/files` could be mounted from the host
    and use `files/` inside gremlin-console to access the files.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- test-build.sh
+++ test-build.sh
@@ -2,5 +2,10 @@
 
 variant="$1"
 
-version=$(grep "$variant" version | sed -E 's/'"$variant"'\s*//')
-docker build -t yihongwang/"$variant" --build-arg COMMIT="$version" "$variant"
+# Make sure the base is already there
+version=$(grep "janusgraph" version | sed -E 's/janusgraph\s*//')
+docker build -t janusgraph --build-arg COMMIT="$version" janusgraph
+
+if [ "$variant" != "" ]; then
+  docker build -t "$variant" "$variant"
+fi
